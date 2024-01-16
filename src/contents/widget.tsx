906d2dcaeb37f75cdc6b0736767a1af8e6b14821
stapleup <tilal.husain@gmail.com>
2024-01-17 00:53:02 +0500

    feat: more tweaks

--- src/contents/widget.tsx
+++ src/contents/widget.tsx
@@ -23,5 +23,5 @@ const CustomButton = () => {
 </Button>
     </Theme>
   }
-   
+
   export default CustomButton
--- src/popup/index.tsx
+++ src/popup/index.tsx
@@ -1,11 +1,23 @@
 import { Theme, Button } from "@radix-ui/themes"
 import React, { useCallback } from "react"
+// import csui from "url:~/content/widget"
 
 import "~assets/style.css"
 
 const Popup = () => {
-  const handleAdd = useCallback(() => {
-    
+  const handleAdd = useCallback(async () => {
+    // const tabs = await chrome.tabs.query({ active: true, currentWindow: true })
+    // chrome.tabs.sendMessage(tabs[0]!.id!, 'launch')
+    // chrome.scripting.executeScript( {
+    //   target: {
+    //     tabId: tabs[0]!.id! // the tab you want to inject into
+    //   },
+    //   world: 'ISOLATED', 
+    //   files: [csui.replace(/chrome-extension:\/\/[a-z]*\/([^?]*)\?.*/i, '$1')], // function to inject
+    // },
+    // () => {
+    //   console.log("Background script got callback after injection")
+    // }) 
   }, [])
 
   return (
